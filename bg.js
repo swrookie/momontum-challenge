727998b22849a1c8978612fd7fd7335edd194a5c
swrookie <44931391+swrookie@users.noreply.github.com>
2021-06-14 02:01:19 +0900

    Update bg.js
    changed background number to 2

--- bg.js
+++ bg.js
@@ -1,28 +1,28 @@
 const body = document.querySelector("body");
-const IMG_NUMBER = 3;
+const IMG_NUMBER = 2;
 
 function handleImageLoad(img) {
-    img.classList.add("bgImg");
-    body.appendChild(img);
+  img.classList.add("bgImg");
+  body.appendChild(img);
 }
 
 function paintImage(imgNum) {
-    const img = new Image();
-    img.src = `images/${imgNum}.jpg`;
-    img.addEventListener("load", handleImageLoad(img));
-    // img.classList.add("bgImg");
-    // body.appendChild(img);
+  const img = new Image();
+  img.src = `images/${imgNum}.jpg`;
+  img.addEventListener("load", handleImageLoad(img));
+  // img.classList.add("bgImg");
+  // body.appendChild(img);
 }
 
 function genRandom() {
-    const number = Math.ceil(Math.random() * IMG_NUMBER);
+  const number = Math.ceil(Math.random() * IMG_NUMBER);
 
-    return number;
+  return number;
 }
 
 function init() {
-    const randNum = genRandom();
-    paintImage(randNum);
+  const randNum = genRandom();
+  paintImage(randNum);
 }
 
 init();
